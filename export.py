dd6459af2449f7c590349875152f6cd73c6805ff
Thomas :) <104996714+twimart@users.noreply.github.com>
2024-12-13 18:01:19 +0100

    Add files via upload

--- export.py
+++ export.py
@@ -4,6 +4,8 @@ import os
 
 def export_vm(template_vm, export_path):
     try:
+        print(f"Début de l'exportation de la VM : {template_vm}")
+        print(f"Chemin d'export : {export_path}")
         # Commande pour exporter la VM en format OVF
         subprocess.run(['VBoxManage', 'export', template_vm, '--output', export_path], check=True)
         print(f"VM {template_vm} exportée avec succès sous {export_path}")
@@ -13,6 +15,7 @@ def export_vm(template_vm, export_path):
 
 def import_vm(ovf_file, clone_name):
     try:
+        print(f"Début de l'importation de la VM depuis : {ovf_file}")
         # Commande pour importer la VM depuis un fichier OVF
         subprocess.run(['VBoxManage', 'import', ovf_file, '--vsys', '0', '--vmname', clone_name], check=True)
         print(f"Clone de la VM créé avec succès : {clone_name}")
@@ -23,24 +26,26 @@ def import_vm(ovf_file, clone_name):
 def clone_vm(template_vm, clone_name, export_dir):
     # Créer un chemin pour le fichier OVF exporté
     export_path = os.path.join(export_dir, f"{clone_name}.ova")
-    
+
+    # Création du répertoire d'export si nécessaire
+    if not os.path.exists(export_dir):
+        print(f"Création du répertoire d'export : {export_dir}")
+        os.makedirs(export_dir)
+
     # Exporter la VM template
     export_vm(template_vm, export_path)
-    
+
     # Importer la VM exportée comme clone
     import_vm(export_path, clone_name)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python clone_vm_export_import.py <nom_de_la_VM_template> <nom_de_la_VM_clone>")
+        print("Usage: python export.py <nom_de_la_VM_template> <nom_de_la_VM_clone>")
         sys.exit(1)
 
     template_vm = sys.argv[1]
     clone_name = sys.argv[2]
-    export_dir = "C:/Users/wimart_t/VirtualBox VMs/exports"  # Répertoire d'exportation (à ajuster selon vos préférences)
+    export_dir = "C:/Users/wimart_t/VirtualBox VMs/exports"  # Répertoire d'exportation
 
-    # Vérifiez si le répertoire d'export existe, sinon créez-le
-    if not os.path.exists(export_dir):
-        os.makedirs(export_dir)
-
+    # Lancer le processus de clonage par export/import
     clone_vm(template_vm, clone_name, export_dir)
